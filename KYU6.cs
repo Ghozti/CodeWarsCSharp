f5352ae97bbf09faea73537ac8b3903ff7f8894d
Ghozti <ybalderas1005@gmail.com>
2021-10-27 11:11:33 -0400

    - added and finished TwoSum

--- KYU6.cs
+++ KYU6.cs
@@ -137,5 +137,22 @@ namespace CodeWarsCSharp
             return tower;
         }
         /********************************************************************************************************************************************************************/
+
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                for(int j= i+1; j < numbers.Length; j++)
+                {
+                    if(numbers[i] + numbers[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+
+            return new int[0];
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,15 +3,7 @@
 namespace CodeWarsCSharp {
     class Program {
         static void Main(string[] args) {
-            
-            
-            for(int i = 0; i < KYU6.TowerBuilder(3).Length; i++)
-            {
-                Console.WriteLine(KYU6.TowerBuilder(3)[i]);
-            }
-            
-
-            //KYU6.TowerBuilder(3);
+            KYU6.TwoSum(new[] { 2, 2, 3 }, 4);
         }
     }
 }
