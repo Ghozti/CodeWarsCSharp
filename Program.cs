e666675814e279f364e2cdbf5bfa5dcb95db11be
ybald <ybalderas1005@gmail.com>
2021-11-20 21:43:34 -0500

    - started working on FoldArray

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@
 namespace CodeWarsCSharp {
     class Program {
         static void Main(string[] args) {
-            
+            for(int i = 0; i < KYU6.FoldArray(new int[] { 1, 2, 3, 4, 5, 6}, 1).Length; i++)
+            {
+                Console.WriteLine(KYU6.FoldArray(new int[] { 1, 2, 3, 4, 5, 6}, 1)[i]);
+            }
         }
     }
 }
